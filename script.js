9244387d5b23fdfab4865721d90705c5b192f72c
SACHIN KUMAR RAJBHER <86919694+sachinrajbher15@users.noreply.github.com>
2024-09-27 00:30:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+inputs.forEach((input, idx) => {
+	input.addEventListener('input', () => {
+		if(input.value.length === 1 && idx < inputs.length-1){
+			inputs[idx+1].focus();
+		}
+	});
+	input.addEventListener('keydown', (event) =>{
+		if(event.key === 'Backspace' && input.value === ''){
+			if(idx>0){
+				inputs[idx-1].focus();
+				inputs[idx-1].value = '';
+			}
+		}
+	});
+});
